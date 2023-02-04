bd351f89e6304d3a80b3fcb655ac6409b46f397e
Saint.JR <1911619172@qq.com>
2023-02-05 01:17:33 +0800

    refactor: 重构整体代码，将MoveBoxUtil内容集成至MovableLinearLayout中

--- core/ui/src/main/java/cc/echarger/echarger/ui/component/MovableLinearLayout.java
+++ core/ui/src/main/java/cc/echarger/echarger/ui/component/MovableLinearLayout.java
@@ -1,23 +1,117 @@
 package cc.echarger.echarger.ui.component;
 
+import android.animation.ValueAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.LinearLayout;
 import androidx.annotation.Nullable;
+import cc.echarger.echarger.ui.util.UnitConversionUtil;
 
 public class MovableLinearLayout extends LinearLayout {
+    private Context context; //上下文
+    private float originY;   //手指初始坐标
+    private float lastY;    //上一个坐标
+    private float boxY;   //模块的起始坐标
+    private float heightUpperBounds; //上限高度
+    private float heightLowerBounds; //下限高度
+    private float offsetHeight; //上下限高度差
+    private boolean isLow = true; //上拉框是否在低位
+//    private TopNaviUtil topNaviUtil;
 
+
+    @SuppressLint("ClickableViewAccessibility")
     public MovableLinearLayout(Context context) {
         super(context);
+        this.context=context;
+        init();
     }
 
     public MovableLinearLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        this.context=context;
+        init();
     }
 
-    public MovableLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    @SuppressLint("ClickableViewAccessibility")
+    private void init(){
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int height = displayMetrics.heightPixels;
+
+        //获取status_bar_height资源的ID
+        @SuppressLint("InternalInsetResource") int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        //状态栏高度
+        int statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+
+        //屏幕高度
+        int screenHeight = height + statusBarHeight;
+
+        heightUpperBounds = screenHeight - UnitConversionUtil.dp2px(context, 700) + statusBarHeight / 2.0f;
+        heightLowerBounds = screenHeight - UnitConversionUtil.dp2px(context, 100 + 20 + 20) + statusBarHeight / 2.0f;
+        offsetHeight = heightLowerBounds - heightUpperBounds;
+
+        this.setY(heightLowerBounds);
+
+        this.setOnTouchListener((view, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN: // 触点按下
+                    boxY = this.getY();
+                    lastY = originY = event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE: // 触点移动
+                    float offsetY = event.getRawY() - lastY;
+                    if (boxY + offsetY >= heightUpperBounds
+                            && boxY + offsetY < heightLowerBounds) {
+                        boxY += offsetY;
+                        this.setY(boxY);
+                    }
+                    lastY = event.getRawY();
+                    break;
+                case MotionEvent.ACTION_UP: // 触点放开
+                    float endY = event.getRawY();
+                    float finalOffsetY = endY - originY;
+                    if (isLow) {
+                        if (-finalOffsetY >= offsetHeight * 0.25) {
+                            isLow = false;
+                            translateAnimation(this.getY(), heightUpperBounds);
+//                            topNaviUtil.foldAnimation(false);
+                        } else {
+                            translateAnimation(this.getY(), heightLowerBounds);
+                        }
+                    } else {
+                        if (finalOffsetY >= offsetHeight * 0.25) {
+                            isLow = true;
+                            translateAnimation(this.getY(), heightLowerBounds);
+//                            topNaviUtil.foldAnimation(true);
+                        } else {
+                            translateAnimation(this.getY(), heightUpperBounds);
+                        }
+                    }
+            }
+            return true;
+        });
+    }
+
+    //    public void setTopNaviUtil(TopNaviUtil topNaviUtil) {
+//        this.topNaviUtil = topNaviUtil;
+//    }
+
+    private void translateAnimation(float startPosition, float endPosition) {
+        ValueAnimator animator = ValueAnimator.ofFloat(startPosition, endPosition);
+        animator.setDuration(300);//播放时长
+
+        animator.addUpdateListener(animation -> {
+            float currentValue = (float) animation.getAnimatedValue();
+            this.setY(currentValue);
+
+            //刷新视图
+            this.requestLayout();
+        });
+        //启动动画
+        animator.start();
     }
 
     @Override
--- core/ui/src/main/java/cc/echarger/echarger/ui/util/MoveBoxUtil.java
+++ core/ui/src/main/java/cc/echarger/echarger/ui/util/MoveBoxUtil.java
@@ -10,95 +10,95 @@ import cc.echarger.echarger.ui.R;
 import cc.echarger.echarger.ui.component.MovableLinearLayout;
 
 public class MoveBoxUtil {
-    private float originY;   //手指初始坐标
-    private float lastY;    //上一个坐标
-    private float boxY;   //模块的起始坐标
-    private final float heightUpperBounds; //上限高度
-    private final float heightLowerBounds; //下限高度
-    private final float offsetHeight; //上下限高度差
-
-    private boolean isLow = true; //上拉框是否在低位
-
-    private TopNaviUtil topNaviUtil;
-
-
-    public void setTopNaviUtil(TopNaviUtil topNaviUtil) {
-        this.topNaviUtil = topNaviUtil;
-    }
-
-    private void translateAnimation(View view, float startPosition, float endPosition) {
-        ValueAnimator animator = ValueAnimator.ofFloat(startPosition, endPosition);
-        animator.setDuration(300);//播放时长
-
-        animator.addUpdateListener(animation -> {
-            float currentValue = (float) animation.getAnimatedValue();
-            view.setY(currentValue);
-
-            //刷新视图
-            view.requestLayout();
-        });
-        //启动动画
-        animator.start();
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    public MoveBoxUtil(Activity context) {
-        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        int height = displayMetrics.heightPixels;
-
-        //获取status_bar_height资源的ID
-        @SuppressLint("InternalInsetResource") int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        //状态栏高度
-        int statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
-
-        //屏幕高度
-        int screenHeight = height + statusBarHeight;
-
-        heightUpperBounds = screenHeight - UnitConversionUtil.dp2px(context, 700) + statusBarHeight / 2.0f;
-        heightLowerBounds = screenHeight - UnitConversionUtil.dp2px(context, 100 + 20 + 20) + statusBarHeight / 2.0f;
-        offsetHeight = heightLowerBounds - heightUpperBounds;
-
-        MovableLinearLayout moveBox = context.findViewById(R.id.move_box);
-        moveBox.setY(heightLowerBounds);
-
-        moveBox.setOnTouchListener((view, event) -> {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN: // 触点按下
-                    boxY = moveBox.getY();
-                    lastY = originY = event.getRawY();
-                    break;
-                case MotionEvent.ACTION_MOVE: // 触点移动
-                    float offsetY = event.getRawY() - lastY;
-                    if (boxY + offsetY >= heightUpperBounds
-                            && boxY + offsetY < heightLowerBounds) {
-                        boxY += offsetY;
-                        moveBox.setY(boxY);
-                    }
-                    lastY = event.getRawY();
-                    break;
-                case MotionEvent.ACTION_UP: // 触点放开
-                    float endY = event.getRawY();
-                    float finalOffsetY = endY - originY;
-                    if (isLow) {
-                        if (-finalOffsetY >= offsetHeight * 0.25) {
-                            isLow = false;
-                            translateAnimation(moveBox, moveBox.getY(), heightUpperBounds);
-                            topNaviUtil.foldAnimation(false);
-                        } else {
-                            translateAnimation(moveBox, moveBox.getY(), heightLowerBounds);
-                        }
-                    } else {
-                        if (finalOffsetY >= offsetHeight * 0.25) {
-                            isLow = true;
-                            translateAnimation(moveBox, moveBox.getY(), heightLowerBounds);
-                            topNaviUtil.foldAnimation(true);
-                        } else {
-                            translateAnimation(moveBox, moveBox.getY(), heightUpperBounds);
-                        }
-                    }
-            }
-            return true;
-        });
-
-    }
+//    private float originY;   //手指初始坐标
+//    private float lastY;    //上一个坐标
+//    private float boxY;   //模块的起始坐标
+//    private final float heightUpperBounds; //上限高度
+//    private final float heightLowerBounds; //下限高度
+//    private final float offsetHeight; //上下限高度差
+//
+//    private boolean isLow = true; //上拉框是否在低位
+//
+//    private TopNaviUtil topNaviUtil;
+//
+//
+//    public void setTopNaviUtil(TopNaviUtil topNaviUtil) {
+//        this.topNaviUtil = topNaviUtil;
+//    }
+//
+//    private void translateAnimation(View view, float startPosition, float endPosition) {
+//        ValueAnimator animator = ValueAnimator.ofFloat(startPosition, endPosition);
+//        animator.setDuration(300);//播放时长
+//
+//        animator.addUpdateListener(animation -> {
+//            float currentValue = (float) animation.getAnimatedValue();
+//            view.setY(currentValue);
+//
+//            //刷新视图
+//            view.requestLayout();
+//        });
+//        //启动动画
+//        animator.start();
+//    }
+//
+//    @SuppressLint("ClickableViewAccessibility")
+//    public MoveBoxUtil(Activity context) {
+//        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+//        int height = displayMetrics.heightPixels;
+//
+//        //获取status_bar_height资源的ID
+//        @SuppressLint("InternalInsetResource") int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+//        //状态栏高度
+//        int statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+//
+//        //屏幕高度
+//        int screenHeight = height + statusBarHeight;
+//
+//        heightUpperBounds = screenHeight - UnitConversionUtil.dp2px(context, 700) + statusBarHeight / 2.0f;
+//        heightLowerBounds = screenHeight - UnitConversionUtil.dp2px(context, 100 + 20 + 20) + statusBarHeight / 2.0f;
+//        offsetHeight = heightLowerBounds - heightUpperBounds;
+//
+//        MovableLinearLayout moveBox = context.findViewById(R.id.move_box);
+//        moveBox.setY(heightLowerBounds);
+//
+//        moveBox.setOnTouchListener((view, event) -> {
+//            switch (event.getAction()) {
+//                case MotionEvent.ACTION_DOWN: // 触点按下
+//                    boxY = moveBox.getY();
+//                    lastY = originY = event.getRawY();
+//                    break;
+//                case MotionEvent.ACTION_MOVE: // 触点移动
+//                    float offsetY = event.getRawY() - lastY;
+//                    if (boxY + offsetY >= heightUpperBounds
+//                            && boxY + offsetY < heightLowerBounds) {
+//                        boxY += offsetY;
+//                        moveBox.setY(boxY);
+//                    }
+//                    lastY = event.getRawY();
+//                    break;
+//                case MotionEvent.ACTION_UP: // 触点放开
+//                    float endY = event.getRawY();
+//                    float finalOffsetY = endY - originY;
+//                    if (isLow) {
+//                        if (-finalOffsetY >= offsetHeight * 0.25) {
+//                            isLow = false;
+//                            translateAnimation(moveBox, moveBox.getY(), heightUpperBounds);
+//                            topNaviUtil.foldAnimation(false);
+//                        } else {
+//                            translateAnimation(moveBox, moveBox.getY(), heightLowerBounds);
+//                        }
+//                    } else {
+//                        if (finalOffsetY >= offsetHeight * 0.25) {
+//                            isLow = true;
+//                            translateAnimation(moveBox, moveBox.getY(), heightLowerBounds);
+//                            topNaviUtil.foldAnimation(true);
+//                        } else {
+//                            translateAnimation(moveBox, moveBox.getY(), heightUpperBounds);
+//                        }
+//                    }
+//            }
+//            return true;
+//        });
+//
+//    }
 }
